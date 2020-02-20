44e7be50bfc49e8d1e243fa0cd720d8b6c29c2ae
ghimani614 <ghimani614@gmail.com>
2020-02-19 19:54:33 -0500

    Player module modified

--- src/com/riskgame/model/Player.java
+++ src/com/riskgame/model/Player.java
@@ -1,19 +1,96 @@
 package com.riskgame.model;
-
 import java.util.List;
 
+/**
+ * This class is used for player module which contains variables for 
+ * name of the Player, Armies it has held etc and methods to change the 
+ * status of the object.
+ * @author Himani
+ * @version 1.0.0.0
+ */
 public class Player {
 	
+	private int player_id;
+	private String player_name;
+	private int armiesHeld;
+	private int armiesFromCards;
 	private Score playerScore;
     private List<Card> cardsHeld;
     private Integer startDiceNo;
-    private String name;
-	public String getName() {
-		return name;
+
+	/**
+	 * The parameterized constructor takes player id and name as parameters
+	 * 
+	 * @param player_id of type integer which is the Id of the player
+	 * @param player_name of type string which is the name of the player
+	 */
+	public Player(int player_id, String player_name) {
+		this.player_id = player_id;
+		this.player_name = player_name;
+		armiesHeld = 0;
+		armiesFromCards = 0;
 	}
-	public void setName(String name) {
-		this.name = name;
+
+	/**
+	 * This method gets the player Id
+	 * @return of type integer, is the id of the player
+	 */
+	public int GetId() {
+		return this.player_id;
 	}
+
+	/**
+	 * This method returns the player name
+	 * @return of type string, is the name of the player
+	 */
+	public String GetName() {
+		return this.player_name;
+	}
+
+	/**
+	 * The method to set the armies of the player
+	 * @param armiesHeld of type integer, will set the armies to the player
+	 */
+	public void SetArmiesToplayer(int armiesHeld) {
+		this.armiesHeld = armiesHeld;
+	}
+
+	/**
+	 * this method gets the number of armies that the player has
+	 * @param player_id is the id of the player
+	 * @return the number of armies of the player
+	 */
+	public int GetArmies() {
+		return this.armiesHeld;
+	}
+
+	/**
+	 * The method to set the player's armies
+	 * @param armiesHeld is the number of new armies
+	 */
+	public void SetArmies(int armiesHeld) {
+		this.armiesHeld = armiesHeld;
+	}
+
+	/**
+	 * this method adds the armies exchanged with cards
+	 * @param armiesFromCards, 
+	 * which is the number of armies that will be added to the player
+	 */
+	public void AddArmiesFromCards(int armiesFromCards) {
+		this.armiesFromCards += armiesFromCards;
+		this.armiesHeld += armiesFromCards;
+	}
+
+	/**
+	 * this method returns the number of armiesFromCards that the player has
+	 * @param player_id, id of the player
+	 * @return the number of armies of the player
+	 */
+	public int GetArmiesFromCards() {
+		return this.armiesFromCards;
+	}
+
 	public Integer getStartDiceNo() {
 		return startDiceNo;
 	}

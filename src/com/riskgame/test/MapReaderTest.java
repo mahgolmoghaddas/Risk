7da448ef0d61513a733236e65144272fd41eb653
Mahgol77 <39947946+mahgolmoghaddas@users.noreply.github.com>
2020-02-20 20:21:22 -0500

    some updates

--- src/com/riskgame/test/MapReaderTest.java
+++ src/com/riskgame/test/MapReaderTest.java
@@ -1,12 +1,30 @@
 package com.riskgame.test;
+import com.riskgame.model.World;
 import com.riskgame.utility.MapReader;
 import org.junit.Before;
+import org.junit.Test;
+import java.io.File;
+import java.net.URL;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class MapReaderTest {
-    public String path="";
-    @Before
-    public void before(){
-        path="user.dir\\map\\World.map";
+    public String path1="map\\World.map";
+    public String path2="map\\WrongMap.map";
+    @Test
+    public void validMapTest() {
+        MapReader map = new MapReader();
+
+
+        assertTrue(map.mapValidity(new World(), path1));
+
     }
+    @Test
+    public void mapg(){
+        MapReader map = new MapReader();
+        assertFalse(map.mapValidity(new World(), path2));
+    }
+
 
 }
